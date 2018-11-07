df38538e8918af2b0515c5e7259fc65e88257ce9
feihu1996x <gsx0312@qq.com>
2018-11-07 18:02:59 +0800

    update

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 const PORT = 8091;
 const WECHAT = {
-    AppID: 'wx3662cd7b42e9a72e',
-    AppSecret: '51285fa6c5d518e4cd3887a3164fcb59',
+    AppID: 'wx7133a141a07ad68e',
+    AppSecret: 'b4e1f2c0893fce01cde621f260960b4b',
     Token: '70pJmQ6EfGkNYc9YhXzCi8l8j42K72eE',    
 };
 const MONGO_HOST = '127.0.0.1';
