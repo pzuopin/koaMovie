31724bb4d9dec2f28d94159b008fe959e623399f
feihu1996x <gsx0312@qq.com>
2018-11-08 17:02:54 +0800

    使用接口测试号

--- config/index.js
+++ config/index.js
@@ -1,8 +1,8 @@
 const PORT = 8091;
 const WECHAT = {
-    AppID: 'wx7133a141a07ad68e',
-    AppSecret: 'b4e1f2c0893fce01cde621f260960b4b',
-    Token: '70pJmQ6EfGkNYc9YhXzCi8l8j42K72eE',    
+    AppID: 'wxd0773ac8ecb445b9',
+    AppSecret: 'c42e33b100b2a9905cba9d6b1eafc109',
+    Token: '70pJmQ6EfGkNYc9YhXzCi8l8j42K72eE',
 };
 const MONGO_HOST = '127.0.0.1';
 const MONGO_PORT = 27017;
